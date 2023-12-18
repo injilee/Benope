813552b1167d68617bd1b4d521cda104553abaa3
Lee Inji <lij8016@gmail.com>
2023-12-18 21:04:03 +0900

    🏗️ Separated sidebar in index.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,11 +3,13 @@ const closeBtn = document.querySelector('.sidebar-close-btn');
 const sidebar = document.querySelector('.sidebar');
 const overlay = document.querySelector('.overlay');
 
+// sidebar 보이기
 function openSidebar() {
   sidebar.classList.add('is-open');
   overlay.classList.add('active');
 }
 
+// sidebar 숨기기
 function closeSidebar() {
   sidebar.classList.remove('is-open');
   overlay.classList.remove('active');
