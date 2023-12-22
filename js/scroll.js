9a6a13c5dbbe0f9129aa8eef8bc910d6dc9fa90d
Lee Inji <lij8016@gmail.com>
2023-12-22 13:52:56 +0900

    🏗️  Separated scroll, top button in index.js

---
+++ js/scroll.js
@@ -0,0 +1,43 @@
+// top btn
+const topBtnWrap = document.querySelector('.top-btn-wrap');
+const topBtn = document.querySelector('.top-btn');
+
+let isHidden = true;
+
+// 이벤트 무한 호출 방지를 위한 throttling
+function throttle(callback, delay = 100) {
+  let isThrottled;
+
+  return (...args) => {
+    if (!isThrottled) {
+      isThrottled = setTimeout(() => {
+        isThrottled = null;
+        callback(...args);
+      }, delay);
+    }
+  };
+}
+
+// 스크롤의 위치에 따라 top-button 보이기/숨기기
+function updateVisibilityButton(scrollY) {
+  if (scrollY >= 400 && isHidden) {
+    topBtnWrap.classList.remove('hidden');
+    isHidden = false;
+  } else if (scrollY < 400 && !isHidden) {
+    topBtnWrap.classList.add('hidden');
+    isHidden = true;
+  }
+}
+
+const throttledScrollHandler = throttle(scrollY => updateVisibilityButton(scrollY));
+
+topBtn.addEventListener('click', function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+  throttledScrollHandler(scrollY);
+});
+
+updateVisibilityButton(window.scrollY);
