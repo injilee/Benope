55def5825989bbc1cbcd8f6b2a5a3b1c2d829690
Lee Inji <lij8016@gmail.com>
2023-12-28 22:23:50 +0900

    🩹 Separate countdown function

---
+++ js/countdown.js
@@ -0,0 +1,31 @@
+// countdown
+const countdownText = document.querySelector('.countdown');
+
+function updateCountdown() {
+  const countdownDate = Date.parse('2024/01/31 24:00:00');
+  const now = new Date();
+  const diff = countdownDate - now;
+
+  const timeDown = Math.floor(diff / 1000);
+
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(diff / (1000 * 60));
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+  const s = seconds - minutes * 60;
+  const m = minutes - hours * 60;
+  const h = hours - days * 24;
+  const d = days;
+
+  if (timeDown <= 0) {
+    clearInterval(timeInterval);
+    countdownText.textContent = '타임 종료';
+  } else {
+    countdownText.textContent = `${d}일 ${h}:${m}:${s} 남음`;
+  }
+}
+
+const timeInterval = setInterval(updateCountdown, 1000);
+
+updateCountdown();
